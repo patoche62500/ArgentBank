94e0829fd5c932bb397312c9fece9b81b72a6037
patoche62500 <72212056+patoche62500@users.noreply.github.com>
2024-02-13 18:36:35 +0100

    fix formulaire & fix local storage

--- frontend/src/components/router/router.jsx
+++ frontend/src/components/router/router.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "../../pages/layout";
 import Home from "../../pages/home/home.jsx";
-import Sign from "../../pages/sign/sign.jsx";
+import Sign, { action as signInAction } from "../../pages/sign/sign.jsx";
 import User from "../../pages/user/user.jsx";
 
 // Configuration du routeur normal pour le site en ligne
@@ -23,6 +23,7 @@ const router = createBrowserRouter([
       {
         path: "/sign",
         element: <Sign />,
+        action: signInAction,
       },
     ],
   },
--- frontend/src/pages/sign/sign.jsx
+++ frontend/src/pages/sign/sign.jsx
@@ -1,31 +1,13 @@
+import { redirect, Form } from "react-router-dom";
 import "./sign.scss";
 
 export default function Sign() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const form = document.querySelector("#formulaire");
-
-    const formData = new FormData(form);
-
-    const data = Object.fromEntries(formData.entries());
-
-    console.log("FormData Object:", data);
-
-    const user = {
-      email: data.username,
-      password: data.password,
-    };
-
-    loginUser(user);
-  };
-
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form id="formulaire" onSubmit={handleSubmit}>
+        <Form id="formulaire" method="post">
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
             <input type="text" id="username" name="username" />
@@ -38,14 +20,32 @@ export default function Sign() {
             <input type="checkbox" id="remember-me" name="rememberMe" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-
           <button type="submit" className="sign-in-button">
             Sign In
           </button>
-        </form>
+        </Form>
       </section>
     </main>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+
+  const data = Object.fromEntries(formData.entries());
+
+  console.log("FormData Object:", data);
+
+  const user = {
+    email: data.username,
+    password: data.password,
+  };
+
+  console.log(request);
+
+  const login = await loginUser(user);
+
+  return login ? redirect("/user") : null;
 }
 
 async function loginUser(user) {
@@ -55,14 +55,18 @@ async function loginUser(user) {
     body: JSON.stringify(user),
   });
 
-  console.log(userData);
+  //console.log(userData);
   const userDataJson = await userData.json();
+  //console.log(userDataJson.body.token);
 
   // response correcte de la par du server
   if (userData.ok) {
-    window.localStorage.setItem("userId", userDataJson.token);
+    window.localStorage.setItem("userId", userDataJson.body.token);
+
+    return true;
     //console.log(userDataJson.userId, userDataJson.token);
   } else {
     //console.log("error login");
+    return false;
   }
 }
