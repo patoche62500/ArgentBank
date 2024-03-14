648ad3b5e0c0329de649ef416d2c303ce9510a1a
patoche62500 <72212056+patoche62500@users.noreply.github.com>
2024-03-14 21:32:56 +0100

    fix image

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
         <Link to={"/"} className="main-nav-logo">
           <img
             className="main-nav-logo-image"
-            src="./img/argentBankLogo.png"
+            src="./img/argentBankLogo.webp"
             alt="Argent Bank Logo"
           />
           <h1 className="sr-only">Argent Bank</h1>
